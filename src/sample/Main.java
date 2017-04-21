bf335addfbe2f4ccacf0dadf49195b785fc3d30d
Ahmed947 <ahmed94essam67@gmail.com>
2017-04-21 18:13:21 +0200

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,12 @@
 package sample;
 
-import sample.mainbody.InstructionFormate;
 import sample.mainbody.PathTwo;
 
 public class Main  {
     public static void main(String[] args) {
 
      PathTwo two = new PathTwo("UpdateCode.txt");
+//        Design.launch();
 
 
     }
--- src/sample/mainbody/Design.java
+++ src/sample/mainbody/Design.java
@@ -1,7 +1,26 @@
-package sample.mainbody;
+/*package sample.mainbody;
 
-/**
- * Created by hp-laptop on 4/13/2017.
- */
-public class Design {
-}
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Design extends Application {
+
+    //this function which choose this class to run th gui
+    public static void launch() {
+        Application.launch(Design.class);
+    }
+    //to load the Fxml file
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/sample.fxml"));
+        Thread.sleep(100);
+        primaryStage.setTitle("Sic\\XE Assmebler");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+}*/
